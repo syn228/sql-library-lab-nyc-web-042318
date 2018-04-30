aeccf7e52ccc0e760b185b613607bd1adb5ebda6
Aaron Na <syn228@nyu.edu>
2018-04-30 17:58:25 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -3,7 +3,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT characters.name, characters.motto FROM characters ORDER BY MAX(LENGTH(characters.motto))"
+  "SELECT characters.name, characters.motto FROM characters ORDER BY LENGTH(characters.motto) DESC LIMIT 1"
 end
 
 
@@ -12,7 +12,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT "
 end
 
 def select_series_title_with_most_human_characters
